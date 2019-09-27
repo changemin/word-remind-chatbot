ab36013b354cfb52ac8a0d434dc1cf79461ea25a
Changemin <bkm.change.min@gmail.com>
2019-09-27 14:48:41 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## Before start
+
+    pip install requests
+    pip install bs4
+    pip install beautifulsoup4
+    pip install lxml
+
+## 프로젝트를 시작하게 된 이유
+
+ 모르는 단어를 매일 찾고 까먹고, 찾고 까먹는 과정은 너무 귀찮았다. naver 사전 크롤링을 활용하면 이를 조금이라도 막을 수 있지 않을까 생각하게 되었다. 
---
+++ main.py
@@ -0,0 +1,17 @@
+import requests, sys
+from bs4 import BeautifulSoup
+
+while(True):
+    word = input("단어를 입력하세요.:>")
+    if word == "@":
+        sys.exit()
+    url = "http://endic.naver.com/search.nhn?query=" + word
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "lxml")
+
+    result = ""
+    try:
+        result += soup.find('dl', {'class':'list_e2'}).find('dd').find('span', {'class':'fnt_k05'}).get_text()
+    except:
+        result = "네이버 사전에 등재되어 있지 않습니다."
+    print(result)
