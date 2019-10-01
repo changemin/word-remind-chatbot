3e2ddc11c9f1aa840da9aa527f79decbb3edf52d
Changemin <bkm.change.min@gmail.com>
2019-10-01 11:58:45 +0900

    new args:[-n]새로운 파일 생성

--- main.py
+++ main.py
@@ -3,40 +3,57 @@ import sys, time, json, os, argparse
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-with open('config.json') as json_file: # read config file
-    data = json.load(json_file)
-    wordFilePath = str(data['Path']['wordFile'])
-    dayInteval = int(data['DATASET']['interval'])
+with open('config.json', 'r') as config: # read config file
+    data = config.read()
+    configData = json.loads(data)
+    targetFile = configData['DATASET']['target']
+    filePath = configData['Words'][targetFile]['Path']
+    dayInterval = configData['DATASET']['interval']
 
 parser = argparse.ArgumentParser() # args parser
 parser.add_argument("-show", help="how json file", action="store_true")
 parser.add_argument("-n", help="create new word space", action="store_true")
-parser.add_argument('FileName', action='store', type=str, help='The text to parse.')
+parser.add_argument("-checkout", help="change file to edit", action="store_true")
+
 args = parser.parse_args()
 
 if args.show: # show config data
     print("-------------------------------------\n")
-    print("WordFilePath : " + wordFilePath)
-    print("dayInterval : " + str(dayInteval) + "\n")
+    print("target : " + targetFile)
+    print("dayInterval : " + str(dayInterval) + "\n")
     print("-------------------------------------")
+    sys.exit()
 
 if args.n: # create new word space
-    newFilePath = "res/word/" + args.FileName + ".txt"
-    newFile = open(newFilePath, "a+", encoding="UTF-8")
+    newFileName = input("새로운 파일의 이름을 입력하세요>")
+    newFilePath = "res/word/" + newFileName + ".txt"
+    try:
+        newFile = open(newFilePath, "x", encoding="UTF-8")
+        print("[LOG] '" + newFilePath + "' 가 생성되었습니다.")
+    except:
+        print("[LOG]" + newFilePath + "가 이미 존재 합니다.")
 
+    sys.exit()
+
+if args.checkout:
+    print("checkout")
+    sys.exit()
+    
 while(True): 
+    configData = json.loads(data) # load json file
     now = datetime.now()
     date_time = now.strftime("%m-%d-%Y/%H:%M")
 
     word = input("단어를 입력하세요>")
-    if word == "exit()":
+
+    if word == "exit()": # interrupt
         sys.exit()
 
     url = "http://endic.naver.com/search.nhn?query=" + word
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "lxml")
 
-    filePath = "res/word/words.txt"
+    
     f = open(filePath, "a+", encoding="UTF-8") # open file append mode
 
     newWord = date_time + ", " + word + ", "
