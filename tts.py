d5a4543d16ca5420ec060b66f931c02843b1a9fb
Changemin <bkm.change.min@gmail.com>
2019-10-10 17:07:16 +0900

    create : tts활용을 위한 tts 파일 생성

---
+++ tts.py
@@ -0,0 +1,23 @@
+import pyttsx3
+import json
+
+with open('config.json', 'r', encoding='UTF-8') as config: # read config file
+    data = config.read()
+    configData = json.loads(data)
+    voiceID_KO = configData['DATASET']['TTS']['voiceID_KO']
+    voiceID_EN = configData['DATASET']['TTS']['voiceID_EN']
+
+engine = pyttsx3.init()
+
+# Voice IDs pulled from engine.getProperty('voices')
+# These will be system specific
+
+# Use female English voice
+engine.setProperty('voice', voiceID_EN)
+engine.say('Hello with my new voice')
+
+# Use female Russian voice
+engine.setProperty('voice', voiceID_KO)
+engine.say('이건 한국어야')
+
+engine.runAndWait()
